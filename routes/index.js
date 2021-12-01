64ade77c94cea54dc27c316cf1104539ec0655b8
KimMalsu <jsm4451@naver.com>
2021-12-01 11:22:56 +0900

    Create signup, submit and Unify header

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,20 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HeavenJosun' });
 });
 
 router.get('/login', (req, res) => {
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: 'HeavenJosun' });
 });
+
+router.get('/signup', (req, res) => {
+  res.render('signup', { title: 'HeavenJosun' });
+});
+
+router.get('/submit', (req, res) => {
+  res.render('submit', { title: 'HeavenJosun' });
+});
+
 
 module.exports = router;
